d095e21d5013c2f9e99125a1f2adfa730c21d758
mjk8v2 <mjk8v2@mail.missouri.edu>
2015-12-06 23:13:04 -0600

    For cmake build system

---
+++ build/CMakeFiles/shell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/shell.dir/src/shell.c.o"
+  "libshell.pdb"
+  "libshell.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/shell_tester.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/shell_tester.dir/test/test.c.o"
+  "shell_tester.pdb"
+  "shell_tester"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/shell_tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
